878a0e8b346c6a33e5030457516dd354e97226f2
ZakharSytoi <zakharsytoi@gmail.com>
2023-07-11 10:35:03 +0200

    main user interface implemented

--- module-main/src/main/java/com/mjc/school/Main.java
+++ module-main/src/main/java/com/mjc/school/Main.java
@@ -1,12 +1,19 @@
 package com.mjc.school;
 
+import com.mjc.school.controller.NewsController;
+import com.mjc.school.service.dtos.NewsDtoRequest;
+import com.mjc.school.service.exception.ValidatorException;
+
 import java.util.Scanner;
+
+import static com.mjc.school.service.exception.ExceptionConstants.INT_VALUE_OUT_OF_RANGE;
 
 public class Main {
     public static void main(String[] args) {
         int operationNumber = 1;
+        NewsController controller = new NewsController();
         Scanner scanner = new Scanner(System.in);
-        do{
+        do {
             System.out.println("""
                     Enter the number of operation:
                     1 - Get all news.
@@ -15,38 +22,96 @@ public class Main {
                     4 - Update news.
                     5 - Remove news by id.
                     0 - Exit.""");
-            if(scanner.hasNextInt()){
+            if (scanner.hasNextInt()) {
                 operationNumber = scanner.nextInt();
-            }
-            else{
+            } else {
                 scanner.nextLine();
                 System.out.println("Command not found.");
                 continue;
             }
-            switch (operationNumber) {
-                case 1 -> {
-                    System.out.println("All news");
+            try {
+                switch (operationNumber) {
+                    case 1 -> {
+                        System.out.println("All news");
+                        controller.getAll().forEach(System.out::println);
+                    }
+                    case 2 -> {
+                        scanner = new Scanner(System.in);
+                        System.out.println("Enter id");
+                        int id;
+                        if (scanner.hasNextInt()) {
+                            id = scanner.nextInt();
+                            System.out.println(controller.getById(id));
+                        } else {
+                            throw new ValidatorException(String.format(INT_VALUE_OUT_OF_RANGE.getCodeMsg()));
+                        }
+                    }
+                    case 3 -> {
+                        scanner = new Scanner(System.in);
+                        System.out.println("creating news");
+                        System.out.println("Enter title");
+                        String title = scanner.nextLine();
+                        System.out.println("Enter content");
+                        String content = scanner.nextLine();
+                        System.out.println("Enter Author id");
+                        long id;
+                        if (scanner.hasNextInt()) {
+                            id = scanner.nextInt();
+                            System.out.println(controller.create(new NewsDtoRequest(1L, title, content, id)));
+                        } else {
+                            throw new ValidatorException(String.format(INT_VALUE_OUT_OF_RANGE.getCodeMsg()));
+                        }
+
+
+                    }
+                    case 4 -> {
+                        scanner = new Scanner(System.in);
+                        System.out.println("updating news");
+                        System.out.println("Enter id");
+                        long id;
+                        if (scanner.hasNextInt()) {
+                            id = scanner.nextInt();
+                        } else {
+                            throw new ValidatorException(String.format(INT_VALUE_OUT_OF_RANGE.getCodeMsg()));
+                        }
+                        System.out.println("Enter title");
+                        scanner.nextLine();
+                        String title = scanner.nextLine();
+                        System.out.println("Enter content");
+                        String content = scanner.nextLine();
+                        System.out.println("Enter Author id");
+                        long authId;
+                        if (scanner.hasNextInt()) {
+                            authId = scanner.nextInt();
+                            System.out.println(controller.update(new NewsDtoRequest(id, title, content, authId)));
+                        } else {
+                            throw new ValidatorException(String.format(INT_VALUE_OUT_OF_RANGE.getCodeMsg()));
+                        }
+
+
+                    }
+                    case 5 -> {
+                        scanner = new Scanner(System.in);
+                        System.out.println("Enter id");
+                        int id;
+                        if (scanner.hasNextInt()) {
+                            id = scanner.nextInt();
+                            System.out.println(controller.delete(id));
+                        } else {
+                            throw new ValidatorException(String.format(INT_VALUE_OUT_OF_RANGE.getCodeMsg()));
+                        }
+
+                    }
+                    case 0 -> System.out.println("exiting");
+
+                    default -> System.out.println("Unknown command");
+
                 }
-                case 2 -> {
-                    System.out.println("getting news by id");
-                }
-                case 3 -> {
-                    System.out.println("creating news");
-                }
-                case 4 -> {
-                    System.out.println("updating news");
-                }
-                case 5 -> {
-                    System.out.println("removing news");
-                }
-                case 0 -> {
-                    System.out.println("exiting");
-                }
-                default -> {
-                    System.out.println("Unknown command");
-                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
-        }while(operationNumber != 0);
+
+        } while (operationNumber != 0);
 
     }
 }
